b73e48b59847364fabab01af98403ba41bcda7ab
krcw02 <krcw02@gmail.com>
2021-01-18 17:56:10 +0100

    first commit

---
+++ Aplikacje desktopowe i mobilne/Zadanie 11/Z1.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Z1 {
+    public static void main(String[] args) {
+
+        int n = 0;
+        boolean poprawna = false;
+        Scanner in = new Scanner(System.in);
+        do {
+            try {
+                poprawna = false;
+                System.out.println("Wprowadz wartosc: ");
+                n = in.nextInt();
+            } catch (Exception e) {
+                System.out.println("Nieprawidlowa wartosc!");
+                in.next();
+                poprawna = true;
+            }
+        } while (poprawna);
+        boolean[][] a = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (nwd(i + 1, j + 1) == 1) {
+                    a[i][j] = true;
+                    System.out.print(" + ");
+                } else {
+                    a[i][j] = false;
+                    System.out.print(" . ");
+                }
+            }
+            System.out.println("");
+        }
+
+    }
+
+    public static int nwd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return nwd(b, a % b);
+        }
+    }
+}
