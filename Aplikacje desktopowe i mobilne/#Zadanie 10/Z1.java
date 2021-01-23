63932312758b93cf421c0ab297a93cae990ad92e
krcw02 <krcw02@gmail.com>
2021-01-23 20:14:06 +0100

    sortowanie

--- Aplikacje desktopowe i mobilne/#Zadanie 10/Z1.java
+++ Aplikacje desktopowe i mobilne/#Zadanie 10/Z1.java
@@ -1,51 +1,119 @@
-import java.util.Arrays;
 
 public class Z1 {
     public static void main(String[] args) {
+        PomiarCzasu czas = new PomiarCzasu();
+        AlgorytmySortujace sort = new AlgorytmySortujace();
         int[] tab = generatorTab();
-
-        long start = System.nanoTime();
-        int[] tabTestSort = testSort(tab);
-        long stop = System.nanoTime();
-
-        System.out.println("##########");
-        System.out.println("testSort: ");
-        // for(int i = 0; i<tab.length;i++)System.out.print(tabTestSort[i]+" ");
-        System.out.println("Czas wykonywania: "+(stop-start));
-        System.out.println("##########");
+        int[] tabWynik = new int[1000];
 
 
+        System.out.print("Sortowanie babelkowe: ");
+        czas.start();
+        tabWynik = sort.babelkowe(tab);
+        System.out.println(czas.stop());
+        // for (int i =0;i<tabWynik.length;i++) System.out.print(tabWynik[i]+" ");System.out.println("");
+
+        System.out.print("Sortowanie Przez Wybor: ");
+        czas.start();
+        tabWynik = sort.sortowaniePrzezWybor(tab);
+        System.out.println(czas.stop());
+        // for (int i =0;i<tabWynik.length;i++) System.out.print(tabWynik[i]+" ");System.out.println("");
+
+        System.out.print("Sortowanie przez wstawianie: ");
+        czas.start();
+        tabWynik = sort.sortowaniePrzezWstawianie(tab);
+        System.out.println(czas.stop());
+        // for (int i =0;i<tabWynik.length;i++) System.out.print(tabWynik[i]+" ");System.out.println("");
+
+        System.out.print("Sortowanie przez scalanie: ");
+        czas.start();
+        tabWynik = sort.sortowaniePrzerzScalanie(tab);
+        System.out.println(czas.stop());
+        // for (int i =0;i<tabWynik.length;i++) System.out.print(tabWynik[i]+" ");System.out.println("");
+
+        System.out.print("Sortowanie szybkie: ");
+        czas.start();
+        tabWynik = sort.sortowanieSzybkie(tab);
+        System.out.println(czas.stop());
+        // for (int i =0;i<tabWynik.length;i++) System.out.print(tabWynik[i]+" ");System.out.println("");
 
     }
-    static int[] generatorTab(){
+
+    static int[] generatorTab() {
         int ilosc = 1000;
-        
+
         int[] tab = new int[ilosc];
-        for(int i = 0; i<tab.length;i++){
-            tab[i] = (int)(Math.random()*100000);
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = (int) (Math.random() * 100000);
+        }
+        return tab;
+    }
+
+}
+
+class PomiarCzasu {
+    long first = 0L;
+
+    public void start() {
+        first = System.nanoTime();
+    }
+
+    public int stop() {
+        long end = System.nanoTime();
+        return (int) (end - first);
+    }
+}
+
+class AlgorytmySortujace {
+
+    // tablice pomocnicze
+    int[] d;
+    int[] p;
+
+    public int[] babelkowe(int[] tab) {
+
+        int x;
+        for (int i = 0; i < tab.length - 1; i++) {
+            for (int j = 0; j < tab.length - 1; j++) {
+                if (tab[j] > tab[j + 1]) {
+                    x = tab[j];
+                    tab[j] = tab[j + 1];
+                    tab[j + 1] = x;
+                }
+            }
         }
 
         return tab;
     }
-    static int[] testSort(int[] tab){
 
-        Arrays.sort(tab);
+    public int[] sortowaniePrzezWybor(int[] tab) {
+        int N = tab.length;
+        for (int j = 0; j < N - 1; j++) {
+            int p = j;
+            for (int i = j + 1; i < N; i++)
+                if (tab[i] < tab[p])
+                    p = i;
+            int x = tab[p];
+            tab[p] = tab[j];
+            tab[j] = x;
+        }
+        return tab;
 
+    }
+
+    public int[] sortowaniePrzezWstawianie(int[] tab) {
+        for (int j = tab.length - 2; j >= 0; j--) {
+            int x = tab[j];
+            int i = j + 1;
+            while ((i < tab.length) && (x > tab[i])) {
+                tab[i - 1] = tab[i];
+                i++;
+            }
+            tab[i - 1] = x;
+        }
         return tab;
     }
-    static int[] babelkowe(int[] tab){
 
-        int x;
-        for (int i = 0; i < tab.length - 1; i++){       
-            for (int j = 0; j < tab.length - 1; j++){
-                if(tab[j] > tab[j + 1]){
-                    x = tab[j];
-                    tab[j] = tab[j + 1];
-                    tab[j + 1] = x;
-                    }
-                 }
-           }    
+    // Aby odblokowac dostep do algorytmow rekurecyjnych wyslij blik na 537848165 10zl w tytule email.
 
-        return tab;
-    }
 }
