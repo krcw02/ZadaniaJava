ea65e22ac303d2ef092cb028699f0c8d069d0bda
krcw02 <krcw02@gmail.com>
2021-01-25 10:19:58 +0100

    zadanie12

--- Aplikacje desktopowe i mobilne/Zadanie 10/Z1.java
+++ Aplikacje desktopowe i mobilne/Zadanie 10/Z1.java
@@ -1,11 +1,12 @@
 
+//Fragety kodu które wypisują zawartość tablic zostały zakomentowane, aby zwiększysz przejrzystość wyników.
 public class Z1 {
     public static void main(String[] args) {
         PomiarCzasu czas = new PomiarCzasu();
         AlgorytmySortujace sort = new AlgorytmySortujace();
         int[] tab = generatorTab();
         int[] tabWynik = new int[1000];
-
+        // for (int i =0;i<tab.length;i++) System.out.print(tab[i]+" ");System.out.println("");
 
         System.out.print("Sortowanie babelkowe: ");
         czas.start();
@@ -35,7 +36,7 @@ public class Z1 {
         czas.start();
         tabWynik = sort.sortowanieSzybkie(tab);
         System.out.println(czas.stop());
-        // for (int i =0;i<tabWynik.length;i++) System.out.print(tabWynik[i]+" ");System.out.println("");
+        for (int i =0;i<tabWynik.length;i++) System.out.print(tabWynik[i]+" ");System.out.println("");
 
     }
 
@@ -66,9 +67,6 @@ class PomiarCzasu {
 
 class AlgorytmySortujace {
 
-    // tablice pomocnicze
-    int[] d;
-    int[] p;
 
     public int[] babelkowe(int[] tab) {
 
@@ -114,6 +112,68 @@ class AlgorytmySortujace {
         return tab;
     }
 
-    // Aby odblokowac dostep do algorytmow rekurecyjnych wyslij blik na 537848165 10zl w tytule email.
+    public int[] sortowaniePrzerzScalanie(int[] tab) {
+
+        return mergeSort(tab, 0, 0, 0);
+
+    }
+
+    private int[] mergeSort(int[] tab, int l, int m, int r1) {
+        int lSize = m - l + 1;
+        int rSize = r1 - m;
+        int[] tabL = new int[lSize];
+        int[] tabR = new int[rSize];
+        for (int x = 0; x < lSize; x++)
+            tabL[x] = tab[l + x];
+        for (int y = 0; y < rSize; y++)
+            tabR[y] = tab[m + 1 + y];
+        int indexL = 0;
+        int indexR = 0;
+        int currIndex;
+        for (currIndex = l; indexL < lSize && indexR < rSize; currIndex++) {
+            if (tabL[indexL] <= tabR[indexR])
+                tab[currIndex] = tabL[indexL++];
+            else
+                tab[currIndex] = tabR[indexR++];
+        }
+
+        while (indexL < lSize)
+            tab[currIndex++] = tabL[indexL++];
+
+        while (indexR < rSize)
+            tab[currIndex++] = tabR[indexR++];
+
+        return tab;
+    }
+
+    public int[] sortowanieSzybkie(int[] tab) {
+
+        return quickSort(tab, 0, tab.length-1);
+    }
+
+    private int[] quickSort(int[] tab, int lo, int hi) {
+        int i = lo, j = hi, h;
+        int x = tab[(lo + hi) / 2];
+
+        do {
+            while (tab[i] < x)
+                i++;
+            while (tab[j] > x)
+                j--;
+            if (i <= j) {
+                h = tab[i];
+                tab[i] = tab[j];
+                tab[j] = h;
+                i++;
+                j--;
+            }
+        } while (i <= j);
+
+        if (lo < j)
+            quickSort(tab, lo, j);
+        if (i < hi)
+            quickSort(tab, i, hi);
+        return tab;
+    }
 
 }
